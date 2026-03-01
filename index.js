d33c9d6ea574ff7f0181bb0d4dffa9d4dc840bde
MEGAIDEATH <thedem.80@gmail.com>
2026-03-01 14:03:07 +0300

    a lot of errors

--- index.js
+++ index.js
@@ -1,52 +1,118 @@
 const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
+const sequelizer = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
 
+const Category = sequelizer.define('Category', {
+    Categoryname: {
+        type: DataTypes.STRING,
+         
+    },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+});
 
-let categories = [];
-let products = [];
-let orders = [];
+const Product = sequelizer.define('Product', {
+    Productname: {
+        type: DataTypes.STRING,
+         
+    },
+    price: {
+        type: DataTypes.FLOAT,
+         
+    },
+    categoryId: {
+        type: DataTypes.INTEGER,
+         
+        references: {
+            model: Category,
+            key: 'id'
+        }
+    }
+});
 
-app.post('/api/categories', (req, res) => {
-  const { name } = req.body;
+const Order = sequelizer.define('Order', {
+    orderNumber: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    items: {
+        type: DataTypes.JSON,
+         
+    },
+    subtotal: {
+        type: DataTypes.FLOAT,
+         
+    },
+    tax: {
+        type: DataTypes.FLOAT,
+         
+    },
+    total: {
+        type: DataTypes.FLOAT,
+         
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW
+    }
+});
+
+// Product.belongsTo(Category);
+// Category.hasMany(Product);
+
+sequelizer.sync({ force: true }).then(() => {
+    console.log("Database & tables created!");
+});
+
+app.post('/api/categories', async (req, res) => {
+  const { Categoryname } = req.body;
 
   const newCategory = {
     id: categories.length + 1,
-    name: name
+    Categoryname
   };
 
-  categories.push(newCategory);
+  await Category.create({Categoryname});
 
-  res.status(201).json(newCategory);
+  res.status(201).json(categories);
 });
 
-app.get('/api/categories', (req, res) => {
-  res.json(categories);
+app.get('/api/categories', async  (req, res) => {
+  const categories = await Category.findAll();
+    res.json(categories);
 });
 
-app.post('/api/products', (req, res) => {
-  const { name, price, categoryId } = req.body;
+app.post('/api/products', async (req, res) => {
+  const { Productname, price, categoryId } = req.body;
 
   const newProduct = {
     id: products.length + 1,
-    name,
+    Productname,
     price,
     categoryId
   };
 
-  products.push(newProduct);
-
+  Product.findByPk(item.productId)
+  
+  await Product.create({Productname, price, categoryId});
+  
   res.status(201).json(newProduct);
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
+app.get('/api/products', async (req, res) => {
+  const products = await Product.findAll({ include: Category });
+    res.json(products);
 });
 
-app.post('/api/orders', (req, res) => {
+app.post('/api/orders', async (req, res) => {
   const { items } = req.body;
 
   let subtotal = 0;
@@ -73,13 +139,16 @@ app.post('/api/orders', (req, res) => {
     createdAt: new Date()
   };
 
-  orders.push(newOrder);
+  await Order.create({items, subtotal, tax, total});
 
   res.status(201).json(newOrder);
 });
-app.get('/api/orders', (req, res) => {
+
+app.get('/api/orders', async (req, res) => {
+  const orders = await Order.findAll();
   res.json(orders);
 });
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`); //node index.js to run the server
 });
